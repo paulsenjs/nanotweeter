421247f3d614487e82b8236555aafffb13c27f29
jjc1138 <jjc1138@98c6e778-be20-11dd-9405-3d0920048215>
2008-11-29 15:08:06 +0000

    Added ADT-generated project skeleton.

---
+++ trunk/src/net/jjc1138/android/twitter/TwitterConfig.java
@@ -0,0 +1,13 @@
+package net.jjc1138.android.twitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TwitterConfig extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
