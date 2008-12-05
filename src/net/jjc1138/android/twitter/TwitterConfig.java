cd8e9a7c79d4dbdc08f21214e34ed6c58d7f0be9
jjc1138 <jjc1138@98c6e778-be20-11dd-9405-3d0920048215>
2008-12-05 04:37:31 +0000

    Reverted last revision because it didn't work. Oops.

--- src/net/jjc1138/android/twitter/TwitterConfig.java
+++ src/net/jjc1138/android/twitter/TwitterConfig.java
@@ -6,7 +6,6 @@ import java.text.MessageFormat;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
@@ -144,7 +143,6 @@ public class TwitterConfig extends Activity {
 				if (sound.isChecked()) {
 					MediaPlayer mp = MediaPlayer.create(
 						TwitterConfig.this, R.raw.tweet);
-					mp.setAudioStreamType(AudioManager.STREAM_RING);
 					mp.setOnCompletionListener(new OnCompletionListener() {
 						@Override
 						public void onCompletion(MediaPlayer mp) {
