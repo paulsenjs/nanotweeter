ae2af7df06286b13638946f2b2f72db8a1457477
jjc1138 <jjc1138@98c6e778-be20-11dd-9405-3d0920048215>
2010-08-15 23:03:41 +0000

    Made the boot receiver acquire a wake lock straight away so that the phone won't fall asleep before doing the first sync and setting the alarm.

--- trunk/src/net/jjc1138/android/twitter/BootReceiver.java
+++ trunk/src/net/jjc1138/android/twitter/BootReceiver.java
@@ -8,6 +8,7 @@ public class BootReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		FetcherWakeLock.acquire(context);
 		context.startService(new Intent(context, Fetcher.class));
 	}
 
